95d79ba7e8fd838c3d281c8f4636e54bc925e718
Rafał Paśko <rp.261000@gmail.com>
2019-10-04 22:39:51 +0200

    gameplay

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ io.on('connection', (socket) => {
     })
 
     socket.on('disconnect', data => {
-        console.log(socket.id)
         io.sockets.emit('keys-share', { keys: getSocketsKeys(io.sockets.connected) })
     })
 
@@ -50,6 +49,11 @@ io.on('connection', (socket) => {
         io.sockets.sockets[data.sender].emit('onAccept', data)
         io.sockets.sockets[data.sender].emit('game-begin', whoBegin ? 1 : 2)
         io.sockets.sockets[data.addressee].emit('game-begin', !whoBegin ? 1 : 2)
+    })
+
+    socket.on('place-hit', data => {
+        console.log(data)
+        io.sockets[data.defender].emit('get-hit',data.cord)
     })
 
 })
